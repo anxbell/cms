2ccf2cb7d0330ae2a9772102e456a2262cfe4c9b
anxbell <valerieanabell@gmail.com>
2025-05-03 15:59:09 -0600

    Completed W02  – CMS project setup with header, contacts components, and data

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  standalone: false,
+  templateUrl: './contact-detail.component.html',
+})
+export class ContactDetailComponent {
+  contact: Contact = new Contact(
+    1,
+    'R. Kent Jackson',
+    'jacksonk@byui.edu',
+    '208-496-3771',
+    'assets/images/jacksonk.jpg',
+    null
+  );
+}
